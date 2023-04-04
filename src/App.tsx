83c5dbcfc82aba5505d92e946cb34ab2a14a1cd2
Brok3nPix3l <rjbabala@yahoo.com>
2023-04-03 23:49:51 -0400

    Added missing perks currentPerk state now accepts Perk not just imageSource string Icon image sizes grow to fit at max res

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,117 @@
 import { useEffect, useState } from "react";
-import Icon from "./Icon";
+import shuffleArray from "shuffle-array";
+import { v4 as uuid } from "uuid";
 
+import aNursesCalling from "/IconPerks_aNursesCalling.webp";
 import aceInTheHole from "/IconPerks_aceInTheHole.webp";
 import adrenaline from "/IconPerks_adrenaline.webp";
 import aftercare from "/IconPerks_aftercare.webp";
 import agitation from "/IconPerks_agitation.webp";
-import aNursesCalling from "/IconPerks_aNursesCalling.webp";
+import alert from "/IconPerks_alert.webp";
 import anyMeansNecessary from "/IconPerks_anyMeansNecessary.webp";
 import appraisal from "/IconPerks_appraisal.webp";
-import alert from "/IconPerks_alert.webp";
 import autodidact from "/IconPerks_autodidact.webp";
+import awakenedAwareness from "/IconPerks_awakenedAwareness.webp";
 import backgroundPlayer from "/IconPerks_backgroundPlayer.webp";
 import balancedLanding from "/IconPerks_balancedLanding.webp";
 import bamboozle from "/IconPerks_bamboozle.webp";
 import barbecueAndChilli from "/IconPerks_barbecueAndChilli.webp";
 import beastOfPrey from "/IconPerks_beastOfPrey.webp";
+import betterThanNew from "/IconPerks_betterThanNew.webp";
+import biteTheBullet from "/IconPerks_biteTheBullet.webp";
 import bitterMurmur from "/IconPerks_bitterMurmur.webp";
+import blastMine from "/IconPerks_blastMine.webp";
 import bloodEcho from "/IconPerks_bloodEcho.webp";
-import bloodhound from "/IconPerks_bloodhound.webp";
+import bloodPact from "/IconPerks_bloodPact.webp";
 import bloodRush from "/IconPerks_bloodRush.webp";
 import bloodWarden from "/IconPerks_bloodWarden.webp";
+import bloodhound from "/IconPerks_bloodhound.webp";
 import boilOver from "/IconPerks_boilOver.webp";
 import bond from "/IconPerks_bond.webp";
+import boonCircleOfHealing from "/IconPerks_boonCircleOfHealing.webp";
 import boonDarkTheory from "/IconPerks_boonDarkTheory.webp";
+import boonExponential from "/IconPerks_boonExponential.webp";
+import boonShadowStep from "/IconPerks_boonShadowStep.webp";
 import borrowedTime from "/IconPerks_borrowedTime.webp";
 import botanyKnowledge from "/IconPerks_botanyKnowledge.webp";
 import breakdown from "/IconPerks_breakdown.webp";
 import breakout from "/IconPerks_breakout.webp";
 import brutalStrength from "/IconPerks_brutalStrength.webp";
 import buckleUp from "/IconPerks_buckleUp.webp";
+import builtToLast from "/IconPerks_builtToLast.webp";
 import callOfBrine from "/IconPerks_callOfBrine.webp";
 import calmSpirit from "/IconPerks_calmSpirit.webp";
+import kinship from "/IconPerks_camaraderie.webp";
+import clairvoyance from "/IconPerks_clairvoyance.webp";
+import correctiveAction from "/IconPerks_correctiveAction.webp";
 import corruptIntervention from "/IconPerks_corruptIntervention.webp";
 import coulrophobia from "/IconPerks_coulrophobia.webp";
+import counterForce from "/IconPerks_counterforce.webp";
 import coupDeGrace from "/IconPerks_coupDeGrace.webp";
+import claustrophobia from "/IconPerks_cruelLimits.webp";
 import cutLoose from "/IconPerks_cutLoose.webp";
 import danceWithMe from "/IconPerks_danceWithMe.webp";
 import darkDevotion from "/IconPerks_darkDevotion.webp";
-import darknessRevealed from "/IconPerks_darknessRevealed.webp";
 import darkSense from "/IconPerks_darkSense.webp";
+import darknessRevealed from "/IconPerks_darknessRevealed.webp";
 import deadHard from "/IconPerks_deadHard.webp";
+import deadMansSwitch from "/IconPerks_deadMansSwitch.webp";
+import deadlock from "/IconPerks_deadlock.webp";
+import deathbound from "/IconPerks_deathbound.webp";
 import deception from "/IconPerks_deception.webp";
 import decisiveStrike from "/IconPerks_decisiveStrike.webp";
 import deerstalker from "/IconPerks_deerstalker.webp";
 import dejaVu from "/IconPerks_dejaVu.webp";
 import deliverance from "/IconPerks_deliverance.webp";
+import desperateMeasures from "/IconPerks_desperateMeasures.webp";
 import detectivesHunch from "/IconPerks_detectivesHunch.webp";
 import discordance from "/IconPerks_discordance.webp";
 import dissolution from "/IconPerks_dissolution.webp";
 import distortion from "/IconPerks_distortion.webp";
 import distressing from "/IconPerks_distressing.webp";
 import diversion from "/IconPerks_diversion.webp";
+import dragonsGrip from "/IconPerks_dragonsGrip.webp";
 import dyingLight from "/IconPerks_dyingLight.webp";
 import empathicConnection from "/IconPerks_empathicConnection.webp";
 import empathy from "/IconPerks_empathy.webp";
 import enduring from "/IconPerks_enduring.webp";
+import eruption from "/IconPerks_eruption.webp";
+import fastTrack from "/IconPerks_fastTrack.webp";
 import fireUp from "/IconPerks_fireUp.webp";
+import selfAware from "/IconPerks_fixated.webp";
+import flashbang from "/IconPerks_flashbang.webp";
 import flipFlop from "/IconPerks_flip-Flop.webp";
 import fogwise from "/IconPerks_fogwise.webp";
+import forThePeople from "/IconPerks_forThePeople.webp";
+import forcedPenance from "/IconPerks_forcedPenance.webp";
 import franklinsDemise from "/IconPerks_franklinsDemise.webp";
 import friendlyCompetition from "/IconPerks_friendlyCompetition.webp";
 import furtiveChase from "/IconPerks_furtiveChase.webp";
 import gameAFoot from "/IconPerks_gameAfoot.webp";
+import gearhead from "/IconPerks_gearhead.webp";
+import grimEmbrace from "/IconPerks_grimEmbrace.webp";
 import guardian from "/IconPerks_guardian.webp";
 import hangmansTrick from "/IconPerks_hangmansTrick.webp";
 import headOn from "/IconPerks_headOn.webp";
+import hexBloodFavour from "/IconPerks_hexBloodFavour.webp";
+import hexCrowdControl from "/IconPerks_hexCrowdControl.webp";
 import hexDevourHope from "/IconPerks_hexDevourHope.webp";
 import hexFaceTheDarkness from "/IconPerks_hexFaceTheDarkness.webp";
 import hexHauntedGround from "/IconPerks_hexHauntedGround.webp";
 import hexHuntressLullaby from "/IconPerks_hexHuntressLullaby.webp";
 import hexNoOneEscapesDeath from "/IconPerks_hexNoOneEscapesDeath.webp";
+import hexPentimento from "/IconPerks_hexPentimento.webp";
+import hexPlaything from "/IconPerks_hexPlaything.webp";
+import hexRetribution from "/IconPerks_hexRetribution.webp";
 import hexRuin from "/IconPerks_hexRuin.webp";
 import hexTheThirdSeal from "/IconPerks_hexTheThirdSeal.webp";
 import hexThrillOfTheHunt from "/IconPerks_hexThrillOfTheHunt.webp";
+import hexUndying from "/IconPerks_hexUndying.webp";
 import hoarder from "/IconPerks_hoarder.webp";
 import hope from "/IconPerks_hope.webp";
 import hubris from "/IconPerks_hubris.webp";
+import hyperfocus from "/IconPerks_hyperfocus.webp";
+import hysteria from "/IconPerks_hysteria.webp";
 import imAllEars from "/IconPerks_imAllEars.webp";
 import infectiousFright from "/IconPerks_infectiousFright.webp";
 import innerFocus from "/IconPerks_innerFocus.webp";
@@ -88,24 +124,30 @@ import kindred from "/IconPerks_kindred.webp";
 import knockOut from "/IconPerks_knockOut.webp";
 import leader from "/IconPerks_leader.webp";
 import leftBehind from "/IconPerks_leftBehind.webp";
+import lethalPursuer from "/IconPerks_lethalPursuer.webp";
 import leverage from "/IconPerks_leverage.webp";
 import lightborn from "/IconPerks_lightborn.webp";
 import lightweight from "/IconPerks_lightweight.webp";
 import lithe from "/IconPerks_lithe.webp";
+import lowProfile from "/IconPerks_lowProfile.webp";
 import luckyBreak from "/IconPerks_luckyBreak.webp";
 import madGrid from "/IconPerks_madGrit.webp";
 import makeYourChoice from "/IconPerks_makeYourChoice.webp";
 import mercilessStorm from "/IconPerks_mercilessStorm.webp";
 import mettleOfMan from "/IconPerks_mettleOfMan.webp";
+import fearmonger from "/IconPerks_mindbreaker.webp";
 import monitorAndAbuse from "/IconPerks_monitorAndAbuse.webp";
 import nemesis from "/IconPerks_nemesis.webp";
 import noMither from "/IconPerks_noMither.webp";
 import noOneLeftBehind from "/IconPerks_noOneLeftBehind.webp";
+import noWayOut from "/IconPerks_noWayOut.webp";
 import nowhereToHide from "/IconPerks_nowhereToHide.webp";
 import objectOfObsession from "/IconPerks_objectOfObsession.webp";
+import offTheRecord from "/IconPerks_offTheRecord.webp";
 import openHanded from "/IconPerks_open-Handed.webp";
 import oppression from "/IconPerks_oppression.webp";
 import overcharge from "/IconPerks_overcharge.webp";
+import overcome from "/IconPerks_overcome.webp";
 import overwhelmingPresence from "/IconPerks_overwhelmingPresence.webp";
 import overzealous from "/IconPerks_overzealous.webp";
 import parentalGuidance from "/IconPerks_parentalGuidance.webp";
@@ -122,59 +164,77 @@ import proveThyself from "/IconPerks_proveThyself.webp";
 import quickAndQuiet from "/IconPerks_quickAndQuiet.webp";
 import quickGambit from "/IconPerks_quickGambit.webp";
 import rancor from "/IconPerks_rancor.webp";
+import reactiveHealing from "/IconPerks_reactiveHealing.webp";
+import reassurance from "/IconPerks_reassurance.webp";
+import redHerring from "/IconPerks_redHerring.webp";
 import rememberMe from "/IconPerks_rememberMe.webp";
 import renewal from "/IconPerks_renewal.webp";
+import repressedAliance from "/IconPerks_repressedAlliance.webp";
 import residualManifest from "/IconPerks_residualManifest.webp";
 import resilience from "/IconPerks_resilience.webp";
+import resurgence from "/IconPerks_resurgence.webp";
+import rookieSpirit from "/IconPerks_rookieSpirit.webp";
 import saboteur from "/IconPerks_saboteur.webp";
 import saveTheBestForLast from "/IconPerks_saveTheBestForLast.webp";
 import scourgeHookFloodsOfRage from "/IconPerks_scourgeHookFloodsOfRage.webp";
+import scourgeHookGiftOfPain from "/IconPerks_scourgeHookGiftOfPain.webp";
 import scourgeHookMonstrousShrine from "/IconPerks_scourgeHookMonstrousShrine.webp";
+import scourgeHookPainResonance from "/IconPerks_scourgeHookPainResonance.webp";
 import selfCare from "/IconPerks_self-Care.webp";
+import selfPreservation from "/IconPerks_self-Preservation.webp";
 import septicTouch from "/IconPerks_septicTouch.webp";
 import shadowborn from "/IconPerks_shadowborn.webp";
 import shatteredHope from "/IconPerks_shatteredHope.webp";
+import situationalAwareness from "/IconPerks_situationalAwareness.webp";
+import slipperyMeat from "/IconPerks_slipperyMeat.webp";
 import sloppyButcher from "/IconPerks_sloppyButcher.webp";
 import smallGame from "/IconPerks_smallGame.webp";
+import smashHit from "/IconPerks_smashHit.webp";
 import soleSurvivor from "/IconPerks_soleSurvivor.webp";
 import solidarity from "/IconPerks_solidarity.webp";
+import soulGuard from "/IconPerks_soulGuard.webp";
 import spiesFromTheShadows from "/IconPerks_spiesFromTheShadows.webp";
 import spineChill from "/IconPerks_spineChill.webp";
 import spiritFury from "/IconPerks_spiritFury.webp";
 import sprintBurst from "/IconPerks_sprintBurst.webp";
 import stakeOut from "/IconPerks_stakeOut.webp";
+import starstruck from "/IconPerks_starstruck.webp";
 import streetwise from "/IconPerks_streetwise.webp";
 import stridor from "/IconPerks_stridor.webp";
+import superiorAnatomy from "/IconPerks_superiorAnatomy.webp";
+import jolt from "/IconPerks_surge.webp";
 import surveillance from "/IconPerks_surveillance.webp";
 import teamworkCollectiveStealth from "/IconPerks_teamworkCollectiveStealth.webp";
 import teamworkPowerOfTwo from "/IconPerks_teamworkPowerOfTwo.webp";
 import technician from "/IconPerks_technician.webp";
 import tenacity from "/IconPerks_tenacity.webp";
+import terminus from "/IconPerks_terminus.webp";
 import territorialImperitive from "/IconPerks_territorialImperative.webp";
 import thanatophobia from "/IconPerks_thanatophobia.webp";
 import thisIsNotHappening from "/IconPerks_thisIsNotHappening.webp";
 import thrillingTremors from "/IconPerks_thrillingTremors.webp";
 import thwack from "/IconPerks_thwack.webp";
 import tinkerer from "/IconPerks_tinkerer.webp";
+import trailOfTorment from "/IconPerks_trailOfTorment.webp";
 import unbreakable from "/IconPerks_unbreakable.webp";
 import unnervingPresence from "/IconPerks_unnervingPresence.webp";
 import unrelenting from "/IconPerks_unrelenting.webp";
 import upTheAnte from "/IconPerks_upTheAnte.webp";
 import urbanEvasion from "/IconPerks_urbanEvasion.webp";
 import vigil from "/IconPerks_vigil.webp";
+import visionary from "/IconPerks_visionary.webp";
 import wakeUp from "/IconPerks_wakeUp.webp";
 import wellMakeIt from "/IconPerks_wellMakeIt.webp";
 import wereGonnaLiveForever from "/IconPerks_wereGonnaLiveForever.webp";
 import whispers from "/IconPerks_whispers.webp";
 import windowsOfOpportunity from "/IconPerks_windowsOfOpportunity.webp";
+import wiretap from "/IconPerks_wiretap.webp";
 import zanshinTactics from "/IconPerks_zanshinTactics.webp";
 
-import { ICON_TRANSFORMATION } from "./Icon";
-import { v4 as uuid } from "uuid";
-import shuffleArray from "shuffle-array";
+import Icon, { ICON_TRANSFORMATION } from "./Icon";
+import Settings from "./Settings";
 import darkMode from "./images/dark-mode.png";
 import lightMode from "./images/light-mode.png";
-import Settings from "./Settings";
 
 export type PerksSetting = "all" | "survivor" | "killer";
 export type ColorTheme = "light" | "dark";
@@ -185,6 +245,67 @@ type Perk = {
 };
 
 const perksArray: Perk[] = [
+  { imageSource: slipperyMeat, role: "survivor" },
+  { imageSource: kinship, role: "survivor" },
+  { imageSource: lowProfile, role: "survivor" },
+  { imageSource: reactiveHealing, role: "survivor" },
+  { imageSource: wiretap, role: "survivor" },
+  { imageSource: selfPreservation, role: "survivor" },
+  { imageSource: smashHit, role: "survivor" },
+  { imageSource: fastTrack, role: "survivor" },
+  { imageSource: builtToLast, role: "survivor" },
+  { imageSource: desperateMeasures, role: "survivor" },
+  { imageSource: visionary, role: "survivor" },
+  { imageSource: forThePeople, role: "survivor" },
+  { imageSource: redHerring, role: "survivor" },
+  { imageSource: offTheRecord, role: "survivor" },
+  { imageSource: selfAware, role: "survivor" },
+  { imageSource: situationalAwareness, role: "survivor" },
+  { imageSource: repressedAliance, role: "survivor" },
+  { imageSource: bloodPact, role: "survivor" },
+  { imageSource: soulGuard, role: "survivor" },
+  { imageSource: boonExponential, role: "survivor" },
+  { imageSource: correctiveAction, role: "survivor" },
+  { imageSource: overcome, role: "survivor" },
+  { imageSource: boonShadowStep, role: "survivor" },
+  { imageSource: boonCircleOfHealing, role: "survivor" },
+  { imageSource: clairvoyance, role: "survivor" },
+  { imageSource: blastMine, role: "survivor" },
+  { imageSource: resurgence, role: "survivor" },
+  { imageSource: counterForce, role: "survivor" },
+  { imageSource: rookieSpirit, role: "survivor" },
+  { imageSource: flashbang, role: "survivor" },
+  { imageSource: biteTheBullet, role: "survivor" },
+  { imageSource: hyperfocus, role: "survivor" },
+  { imageSource: reassurance, role: "survivor" },
+  { imageSource: betterThanNew, role: "survivor" },
+  { imageSource: fearmonger, role: "killer" },
+  { imageSource: claustrophobia, role: "killer" },
+  { imageSource: jolt, role: "killer" },
+  { imageSource: terminus, role: "killer" },
+  { imageSource: awakenedAwareness, role: "killer" },
+  { imageSource: superiorAnatomy, role: "killer" },
+  { imageSource: hexPentimento, role: "killer" },
+  { imageSource: scourgeHookPainResonance, role: "killer" },
+  { imageSource: grimEmbrace, role: "killer" },
+  { imageSource: scourgeHookGiftOfPain, role: "killer" },
+  { imageSource: hexPlaything, role: "killer" },
+  { imageSource: deadlock, role: "killer" },
+  { imageSource: eruption, role: "killer" },
+  { imageSource: hysteria, role: "killer" },
+  { imageSource: lethalPursuer, role: "killer" },
+  { imageSource: noWayOut, role: "killer" },
+  { imageSource: hexCrowdControl, role: "killer" },
+  { imageSource: starstruck, role: "killer" },
+  { imageSource: hexUndying, role: "killer" },
+  { imageSource: hexBloodFavour, role: "killer" },
+  { imageSource: dragonsGrip, role: "killer" },
+  { imageSource: deathbound, role: "killer" },
+  { imageSource: trailOfTorment, role: "killer" },
+  { imageSource: forcedPenance, role: "killer" },
+  { imageSource: hexRetribution, role: "killer" },
+  { imageSource: deadMansSwitch, role: "killer" },
+  { imageSource: gearhead, role: "killer" },
   { imageSource: aceInTheHole, role: "survivor" },
   { imageSource: adrenaline, role: "survivor" },
   { imageSource: aftercare, role: "survivor" },
@@ -376,10 +497,10 @@ function App() {
     if (perksSetting === "all") newPerks = shuffleArray(perksArray);
     if (perksSetting === "survivor") newPerks = shuffleArray(survivorPerks);
     if (perksSetting === "killer") newPerks = shuffleArray(killerPerks);
-    setCurrentPerk(newPerks[0].imageSource);
+    setCurrentPerk(newPerks[0]);
     setPerks(newPerks);
   };
-  const [currentPerk, setCurrentPerk] = useState<string>(perks[0].imageSource);
+  const [currentPerk, setCurrentPerk] = useState<Perk>(perks[0]);
 
   const resetGame = () => {
     console.log("resetting game");
@@ -403,9 +524,13 @@ function App() {
     return perk.role === "killer";
   });
 
-  const nextPerk = perks[nextPerkIndex].imageSource;
+  const nextPerk = perks[nextPerkIndex];
   useEffect(() => {
-    new Image().src = nextPerk;
+    if (!nextPerk) {
+      console.log("No next perk found. Are you debugging?");
+      return;
+    }
+    new Image().src = nextPerk.imageSource;
     console.log(nextPerk);
   }, [nextPerk]);
 
@@ -513,7 +638,7 @@ function App() {
               (transform: ICON_TRANSFORMATION) => {
                 return (
                   <Icon
-                    imageSrc={currentPerk}
+                    imageSrc={currentPerk.imageSource}
                     handleSelectedIcon={handleSelectedIcon}
                     transform={transform}
                     key={uuid()}
--- src/Icon.tsx
+++ src/Icon.tsx
@@ -15,7 +15,7 @@ export default function Icon(props: IconProps) {
     <div className="flex justify-center border-4 border-slate-900 bg-slate-700 dark:border-stone-200 dark:bg-stone-700">
       <img
         src={props.imageSrc}
-        className={`cursor-pointer
+        className={`cursor-pointer min-[550px]:w-[256px]
         ${props.transform === "invert-y" && "-scale-x-100"} ${
           props.transform === "rotate 180" && "rotate-180"
         } ${
